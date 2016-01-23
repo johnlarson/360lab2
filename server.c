d71907bcc4f989e85ab113104f3265450ac6000a
John <john.robert.larson@gmail.com>
2016-01-23 21:01:24 +0000

    startSocket function

--- server.c
+++ server.c
@@ -7,27 +7,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int startSocket(int port, int* hSocket, struct sockaddr_in* address);
-void acceptLoop(int hSocket, struct sockaddr_in address);
+#define QUEUE_SIZE 5
+#define ERROR -1
+
+void startSocket(int port, int* hSocket, struct sockaddr_in* address);
+void acceptLoop(int hServerSocket, struct sockaddr_in address);
 void acceptConnection(int hSocket, struct sockaddr_in address);
 void respondRequest(int hSocket);
 void closeSocket(int hSocket);
 
 void runserver(int port, char* dir) {
-	int hSocket;
+	int hServerSocket;
 	struct sockaddr_in address;
-	startSocket(port, &hSocket, &address);
-	acceptLoop(hSocket, address);	
-	closeSocket(hSocket);
+	startSocket(port, &hServerSocket, &address);
+	acceptLoop(hServerSocket, address);	
+	closeSocket(hServerSocket);
 }
 
-int startSocket(int port, int* hSocket, struct sockaddr_in* address) {
-	return 1;
+void startSocket(int port, int* hSocket, struct sockaddr_in* address) {
+	*hSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if(*hSocket == ERROR) {
+		expressFrustration("I couldn't make a socket, and it's probably your fault");
+	}
+	address->sin_addr.s_addr = INADDR_ANY;
+	address->sin_port = htons(port);
+	address->sin_family = AF_INET;
+	if(bind(*hSocket, (struct sockaddr*)address, sizeof(*address)) == ERROR) {
+		expressFrustration("I can't bind the address to the socket");
+	}
+	int addressSize = sizeof(struct sockaddr_in);
+	getsockname(*hSocket, (struct sockaddr*)address, (socklen_t*)&addressSize);
+	if(listen(*hSocket, QUEUE_SIZE) == ERROR) {
+		expressFrustration("I wasn't able to listen on the port you gave me");
+	} 
 }
 
-void acceptLoop(int hSocket, struct sockaddr_in address) {
+void acceptLoop(int hServerSocket, struct sockaddr_in address) {
 	while(1) {
-		acceptConnection(hSocket, address);
+		int addressSize = sizeof(struct sockaddr_in);
+		int hSocket = accept(hServerSocket, (struct sockaddr*)&address, (socklen_t*)&addressSize);
 		respondRequest(hSocket);
 	}
 }
