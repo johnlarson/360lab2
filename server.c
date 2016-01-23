4aaede80fd9c37de2fe646932d539b5e3c1b7461
John <john.robert.larson@gmail.com>
2016-01-23 04:51:42 +0000

    made the skeleton of server.c

--- server.c
+++ server.c
@@ -7,6 +7,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int startSocket(int port, int* hSocket, struct sockaddr_in* address);
+void acceptLoop(int hSocket, struct sockaddr_in address);
+void acceptConnection(int hSocket, struct sockaddr_in address);
+void respondRequest(int hSocket);
+void closeSocket(int hSocket);
+
 void runserver(int port, char* dir) {
+	int hSocket;
+	struct sockaddr_in address;
+	startSocket(port, &hSocket, &address);
+	acceptLoop(hSocket, address);	
+	closeSocket(hSocket);
+}
+
+int startSocket(int port, int* hSocket, struct sockaddr_in* address) {
+	return 1;
+}
+
+void acceptLoop(int hSocket, struct sockaddr_in address) {
+	while(1) {
+		acceptConnection(hSocket, address);
+		respondRequest(hSocket);
+	}
+}
+
+void acceptConnection(int hSocket, struct sockaddr_in address) {
+	
+}
+
+void respondRequest(int hSocket) {
+	
+}
+
+void closeSocket(int hSocket) {
 	
 }
